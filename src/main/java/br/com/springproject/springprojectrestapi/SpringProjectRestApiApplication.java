ad60d24fd0c98a6f553ccb3f8ff14bf9ca0674ce
= <=>
2020-11-03 09:18:12 -0400

    Commit inicial do projecto

---
+++ src/main/java/br/com/springproject/springprojectrestapi/SpringProjectRestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.springproject.springprojectrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springproject/springprojectrestapi/SpringProjectRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springproject.springprojectrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
